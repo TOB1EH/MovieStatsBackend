bbb9d5143e08a128b7f2dc412822dcdf22002dbd
Gastón Zaragosi <gzaragosi@gmail.com>
2025-10-28 15:00:48 -0300

    se empezo con funcionalidades de voto 3

--- src/main/java/com/moviestats/model/business/IVotoBusiness.java
+++ src/main/java/com/moviestats/model/business/IVotoBusiness.java
@@ -1,5 +1,84 @@
 package com.moviestats.model.business;
+import java.util.List;
 
-public class IVotoBussiness {
+import com.moviestats.model.Voto;
+import com.moviestats.model.business.exceptions.*;
 
+/**
+ * Interfaz que define las operaciones de negocio para la gestión de Votos.
+ * <p>
+ * Incluye métodos para listar, buscar, agregar, actualizar y eliminar Votos,
+ * manejando las excepciones correspondientes a reglas de negocio y existencia de datos.
+ * </p>
+ */
+
+public interface IVotoBusiness {
+    /**
+     * Obtiene la lista completa de Votos.
+     *
+     * @return Lista de Votos existentes.
+     * @throws BusinessException Si ocurre un error general en la lógica de negocio.
+     */
+    public List<Voto> list() throws BusinessException;
+
+     /**
+     * Carga un Voto específico a partir de su identificador único.
+     *
+     * @param idPelicula Identificador las peliculas a cargar.
+     * @return Voto correspondiente al identificador proporcionado.
+     * @throws BusinessException Si ocurre un error en la lógica de negocio.
+     * @throws NotFoundException  Si no se encuentra un Voto con el identificador dado.
+     */
+    public Double loadAVG(Long idPelicula) throws BusinessException, NotFoundException;
+
+
+         /**
+     * Carga un Voto específico a partir de su identificador único.
+     *
+     * @param idPelicula Identificador las peliculas a cargar.
+     * @return Voto correspondiente al identificador proporcionado.
+     * @throws BusinessException Si ocurre un error en la lógica de negocio.
+     * @throws NotFoundException  Si no se encuentra un Voto con el identificador dado.
+     */
+    public Voto load(Long id) throws BusinessException, NotFoundException;
+
+    /**
+     * Carga 
+     *
+     * @param Voto Nombre o descripción del Voto a cargar.
+     * @return Voto correspondiente al nombre proporcionado.
+     * @throws BusinessException Si ocurre un error en la lógica de negocio.
+     * @throws NotFoundException  Si no se encuentra un Voto con el nombre dado.
+     */
+    public Voto loadUsuario(Long idUsuario, Long idPelicula) throws BusinessException, NotFoundException;
+
+    /**
+     * Agrega un nuevo Voto al sistema.
+
+     * @param Voto Voto a agregar.
+     * @return Voto agregado, incluyendo su identificador generado.
+     * @throws BusinessException Si ocurre un error en la lógica de negocio.
+     * @throws FoundException    Si ya existe un Voto igual en el sistema.
+     */
+    public Voto add(Voto voto) throws BusinessException, FoundException;
+
+    /**
+     * Actualiza los datos de un Voto existente.
+     *
+     * @param Voto Voto con los datos actualizados.
+     * @return Voto actualizado.
+     * @throws BusinessException Si ocurre un error en la lógica de negocio.
+     * @throws NotFoundException Si el Voto a actualizar no existe.
+     * @throws FoundException Si ya existe un Voto igual en el sistema.
+     */
+    public Voto update(Voto Voto) throws BusinessException, NotFoundException, FoundException;
+
+    /**
+     * Elimina un Voto del sistema a partir de su identificador.
+     *
+     * @param id Identificador del Voto a eliminar.
+     * @throws BusinessException Si ocurre un error en la lógica de negocio.
+     * @throws NotFoundException Si el Voto a eliminar no existe.
+     */
+    public void delete(Long id) throws BusinessException, NotFoundException;
 }
--- src/main/java/com/moviestats/model/persistence/VotoRepository.java
+++ src/main/java/com/moviestats/model/persistence/VotoRepository.java
@@ -1,5 +1,47 @@
 package com.moviestats.model.persistence;
 
-public class VotoRepository {
+import java.util.Optional;
+
+import com.moviestats.model.Voto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repositorio para la gestión de la persistencia de {@link Voto}.
+ * <p>
+ * Extiende {@link JpaRepository} para proporcionar operaciones CRUD básicas y
+ * consultas personalizadas sobre la entidad Voto.
+ * </p>
+ * 
+ * <p>
+ * Anotaciones:
+ * <ul>
+ *   <li>{@code @Repository}: Marca la interfaz como un componente de repositorio de Spring.</li>
+ * </ul>
+ * </p>
+ */
+@Repository
+public interface VotoRepository extends JpaRepository<Voto, Long> {
+    
+    /**
+     * Busca los votos de un usuario
+     *
+     * @param Voto Nombre o descripción del Voto a buscar.
+     * @return {@link Optional} que contiene el Voto si se encuentra, o vacío si no existe.
+     */
+    @Query("SELECT g FROM Voto g WHERE g.idUsuario = :usuario AND g.idPelicula = :pelicula")
+    Optional<Voto> findByIdUsuario(@Param("idUsuario") long usuario, @Param("idPelicula") String pelicula);
+
+    /**
+     * Busca el promedio de una pelicula en base a su id
+     * @param Voto Nombre o descripción del Voto a buscar.
+     * @param id Identificador del Voto a excluir de la búsqueda.
+     * @return {@link Optional} que contiene el Voto si se encuentra, o vacío si no existe.
+     */
+    @Query("SELECT AVG(v.numero_voto) FROM Voto v WHERE v.peliula = :idPelicula")
+    Double obtenerPromedioPorPeliula(@Param("idPelicula") String Voto);
 
 }
