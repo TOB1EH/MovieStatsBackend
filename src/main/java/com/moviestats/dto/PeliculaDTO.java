7eefc380c91ff8b28c8d363a2363baa868d20df5
Tobias Funes <tobiasfunes@hotmail.com.ar>
2025-11-08 13:27:12 -0300

    Solucion problemas al hacer merge de cambios en develop

--- src/main/java/com/moviestats/dto/PeliculaDTO.java
+++ src/main/java/com/moviestats/dto/PeliculaDTO.java
@@ -2,9 +2,18 @@ package com.moviestats.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * DTO simple para exponer datos de película al frontend.
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class PeliculaDTO {
 
     private long id;
@@ -14,63 +23,4 @@ public class PeliculaDTO {
     private String poster;
     private String overview;
     private List<String> genres;
-
-    public PeliculaDTO() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public Float getRating() {
-        return rating;
-    }
-
-    public void setRating(Float rating) {
-        this.rating = rating;
-    }
-
-    public String getPoster() {
-        return poster;
-    }
-
-    public void setPoster(String poster) {
-        this.poster = poster;
-    }
-
-    public String getOverview() {
-        return overview;
-    }
-
-    public void setOverview(String overview) {
-        this.overview = overview;
-    }
-
-    public List<String> getGenres() {
-        return genres;
-    }
-
-    public void setGenres(List<String> genres) {
-        this.genres = genres;
-    }
 }
--- src/main/java/com/moviestats/dto/tmdb/TMDBMovieDTO.java
+++ src/main/java/com/moviestats/dto/tmdb/TMDBMovieDTO.java
@@ -16,44 +16,43 @@ import java.util.List;
 @Setter
 @Getter
 public class TMDBMovieDTO {
-    
     private Long id;
     private String title;
     private String overview;
-    
+
     @JsonProperty("poster_path")
     private String posterPath;
-    
+
     @JsonProperty("backdrop_path")
     private String backdropPath;
-    
+
     @JsonProperty("release_date")
     private String releaseDate;
-    
+
     @JsonProperty("vote_average")
     private Float voteAverage;
-    
+
     @JsonProperty("vote_count")
     private Integer voteCount;
-    
+
     @JsonProperty("original_language")
     private String originalLanguage;
-    
+
     @JsonProperty("original_title")
     private String originalTitle;
-    
+
     @JsonProperty("genre_ids")
     private List<Integer> genreIds;
-    
+
     private List<TMDBGenreDTO> genres;
-    
+
     private Integer runtime;
-    
+
     private String status;
-    
+
     private String tagline;
-    
+
     private Boolean adult;
-    
+
     private Double popularity;
 }
