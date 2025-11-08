cd0cb073386d43446a4386061a4bee3c097d1ec7
Tobias Funes <tobiasfunes@hotmail.com.ar>
2025-11-08 12:31:22 -0300

    Agregado de Setters, Getters y Constructores automaticos de Spring Boot lombok

--- src/main/java/com/moviestats/dto/tmdb/TMDBGenreDTO.java
+++ src/main/java/com/moviestats/dto/tmdb/TMDBGenreDTO.java
@@ -1,34 +1,19 @@
 package com.moviestats.dto.tmdb;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * DTO para representar un género de TMDB.
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class TMDBGenreDTO {
-    
+
     private Integer id;
     private String name;
-    
-    public TMDBGenreDTO() {
-    }
-    
-    public TMDBGenreDTO(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-    
-    public Integer getId() {
-        return id;
-    }
-    
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/com/moviestats/dto/tmdb/TMDBMoviePageDTO.java
+++ src/main/java/com/moviestats/dto/tmdb/TMDBMoviePageDTO.java
@@ -1,52 +1,31 @@
 package com.moviestats.dto.tmdb;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
 /**
  * DTO para mapear la respuesta paginada de películas de TMDB.
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class TMDBMoviePageDTO {
-    
+
     private Integer page;
-    
+
     private List<TMDBMovieDTO> results;
-    
+
     @JsonProperty("total_pages")
     private Integer totalPages;
-    
+
     @JsonProperty("total_results")
     private Integer totalResults;
-    
-    public Integer getPage() {
-        return page;
-    }
-    
-    public void setPage(Integer page) {
-        this.page = page;
-    }
-    
-    public List<TMDBMovieDTO> getResults() {
-        return results;
-    }
-    
-    public void setResults(List<TMDBMovieDTO> results) {
-        this.results = results;
-    }
-    
-    public Integer getTotalPages() {
-        return totalPages;
-    }
-    
-    public void setTotalPages(Integer totalPages) {
-        this.totalPages = totalPages;
-    }
-    
-    public Integer getTotalResults() {
-        return totalResults;
-    }
-    
-    public void setTotalResults(Integer totalResults) {
-        this.totalResults = totalResults;
-    }
 }
+
